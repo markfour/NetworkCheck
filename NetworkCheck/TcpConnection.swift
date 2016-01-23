2cf91d41addfcad766f8ef8de4641658e8b9d959
hayashidakazumi <axhfaro@gmail.com>
2016-01-23 14:47:39 +0900

    Add frame and button

---
+++ NetworkCheck/TcpConnection.swift
@@ -0,0 +1,16 @@
+//
+//  TcpConnection.swift
+//  NetworkCheck
+//
+//  Created by HayashidaKazumi on 2016/01/23.
+//  Copyright © 2016年 HayashidaKazumi. All rights reserved.
+//
+
+import Foundation
+
+class TcpConnection: NSObject {
+  
+  func connection() {
+    
+  }
+}
